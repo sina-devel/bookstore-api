fe98a24d59a3ca31e591dcef96a09eb837881e3d
Kianoosh Ashayeri Zade <kianoosh.az@gmail.com>
2021-04-28 22:51:11 +0430

    add errors package

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -1,1 +1,57 @@
 package errors
+
+import "net/http"
+
+type (
+	kind uint
+
+	serverError struct {
+		kind    kind
+		message string
+	}
+)
+
+const (
+	_ kind = iota
+	KindInvalid
+	KindNotFound
+	KindUnauthorized
+	KindUnexpected
+	KindNotAllowed
+)
+
+var (
+	httpErrors = map[kind]int{
+		KindInvalid:      http.StatusBadRequest,
+		KindNotFound:     http.StatusNotFound,
+		KindUnauthorized: http.StatusUnauthorized,
+		KindUnexpected:   http.StatusInternalServerError,
+		KindNotAllowed:   http.StatusMethodNotAllowed,
+	}
+)
+
+func New(kind kind, msg string) error {
+	return serverError{
+		kind:    kind,
+		message: msg,
+	}
+}
+
+func (e serverError) Error() string {
+	return e.message
+}
+
+func HttpError(err error) (string, int) {
+	serverErr, ok := err.(serverError)
+	if !ok {
+		return err.Error(), http.StatusBadRequest
+	}
+
+	code, ok := httpErrors[serverErr.kind]
+	if !ok {
+		return serverErr.message, http.StatusBadRequest
+	}
+
+	return serverErr.message, code
+
+}
