d5b0297c99e4ead1d3d1aa3d84f9710a721060e0
Kianoosh Ashayeri Zade <kianoosh.az@gmail.com>
2021-04-29 15:27:28 +0430

    fix variable names

--- pkg/errors/errors_test.go
+++ pkg/errors/errors_test.go
@@ -13,34 +13,34 @@ func TestHttpError(t *testing.T) {
 	tests := []struct {
 		name  string
 		args  args
-		want  string
-		want1 int
+		want1 string
+		want2 int
 	}{
 		{
 			name: "server error",
 			args: args{
 				err: New(KindNotFound, "page not found"),
 			},
-			want:  "page not found",
-			want1: http.StatusNotFound,
+			want1: "page not found",
+			want2: http.StatusNotFound,
 		},
 		{
 			name: "other errors",
 			args: args{
 				err: errors.New("something"),
 			},
-			want:  "something",
-			want1: http.StatusBadRequest,
+			want1: "something",
+			want2: http.StatusBadRequest,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, got1 := HttpError(tt.args.err)
-			if got != tt.want {
-				t.Errorf("HttpError() got = %v, want %v", got, tt.want)
-			}
+			got1, got2 := HttpError(tt.args.err)
 			if got1 != tt.want1 {
-				t.Errorf("HttpError() got1 = %v, want %v", got1, tt.want1)
+				t.Errorf("HttpError() got = %v, want %v", got1, tt.want1)
+			}
+			if got2 != tt.want2 {
+				t.Errorf("HttpError() got1 = %v, want %v", got2, tt.want2)
 			}
 		})
 	}
