0e54d18e11f258ca782b5b680ade7c3dab2ddc8c
Kianoosh Ashayeri Zade <kianoosh.az@gmail.com>
2021-05-14 15:14:23 +0430

    add schema method

--- internal/db/postgres/schema/book.go
+++ internal/db/postgres/schema/book.go
@@ -1,6 +1,7 @@
 package schema
 
 import (
+	"github.com/kianooshaz/bookstore-api/internal/models"
 	"github.com/kianooshaz/bookstore-api/internal/models/types"
 	"gorm.io/gorm"
 )
@@ -20,3 +21,42 @@ type (
 		Price         types.Price
 	}
 )
+
+func (b *Book) ConvertModel() *models.Book {
+	var comments []models.Comment
+	for _, comment := range b.Comments {
+		comments = append(comments, models.Comment{
+			ID:          comment.ID,
+			UserID:      comment.UserID,
+			Text:        comment.Text,
+			BookID:      comment.BookID,
+			FullName:    comment.FullName,
+			IsConfirmed: comment.IsConfirmed,
+		})
+	}
+
+	var pictures []models.Picture
+	for _, picture := range b.Pictures {
+		pictures = append(pictures, models.Picture{
+			ID:     picture.ID,
+			Name:   picture.Name,
+			Alt:    picture.Alt,
+			BookID: picture.BookID,
+		})
+	}
+
+	return &models.Book{
+		ID:            b.ID,
+		Name:          b.Name,
+		Description:   b.Description,
+		File:          b.File,
+		SellerID:      b.SellerID,
+		CategoryID:    b.CategoryID,
+		Comments:      comments,
+		DownloadCount: b.DownloadCount,
+		Pictures:      pictures,
+		Status:        b.Status,
+		Price:         b.Price,
+	}
+
+}
