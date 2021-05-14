be3c57731d08f66a1dfb31df48623b10e2d78b17
Kianoosh Ashayeri Zade <kianoosh.az@gmail.com>
2021-05-14 14:14:47 +0430

    add test for isErrorNotFound

---
+++ internal/db/postgres/utils_test.go
@@ -0,0 +1,44 @@
+package postgres
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func TestIsErrorNotFound(t *testing.T) {
+	user := newUserTest()
+	err1 := repoTest.db.First(user, rand.Uint32()).Error
+
+	t.Run("create new record", func(t *testing.T) {
+		if err := repoTest.CreateUser(user); err != nil {
+			t.Fail()
+		}
+	})
+
+	err2 := repoTest.db.First(user, user.ID).Error
+
+	test := []struct {
+		name string
+		err  error
+		want bool
+	}{
+		{
+			name: "error not found",
+			err:  err1,
+			want: true,
+		},
+		{
+			name: "error nil",
+			err:  err2,
+			want: false,
+		},
+	}
+	for _, tt := range test {
+		t.Run(tt.name, func(t *testing.T) {
+			err := isErrorNotFound(tt.err)
+			if err != tt.want {
+				t.Error()
+			}
+		})
+	}
+}
