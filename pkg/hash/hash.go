bb52fef77d5b67f10b293e0eee30e8961078eba7
Kianoosh Ashayeri Zade <kianoosh.az@gmail.com>
2021-05-17 21:43:16 +0430

    add IsErrorInvalidPassword

--- pkg/hash/hash.go
+++ pkg/hash/hash.go
@@ -1,6 +1,9 @@
 package hash
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func Password(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
@@ -10,4 +13,12 @@ func Password(password string) (string, error) {
 func CheckPassword(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+func IsErrorInvalidPassword(err error) bool {
+	if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+		return true
+	}
+
+	return false
 }
